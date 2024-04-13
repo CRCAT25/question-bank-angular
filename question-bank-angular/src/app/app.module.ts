300bb8dbfd631304598db1107d99aa29ceb79262
doquocthanh <doquocthanh@github.com>
2024-04-13 10:22:17 +0700

    question-bank-angular p-app Add: Split main page layout

--- question-bank-angular/src/app/app.module.ts
+++ question-bank-angular/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PHeaderComponent } from './p-app/p-header/p-header.component';
+import { PNavbarComponent } from './p-app/p-navbar/p-navbar.component';
+import { PQuestionbankComponent } from './p-app/p-questionbank/p-questionbank.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PHeaderComponent,
+    PNavbarComponent,
+    PQuestionbankComponent
   ],
   imports: [
     BrowserModule,
---
+++ question-bank-angular/src/app/p-app/p-questionbank/p-questionbank.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-p-questionbank',
+  templateUrl: './p-questionbank.component.html',
+  styleUrl: './p-questionbank.component.scss'
+})
+export class PQuestionbankComponent {
+
+}
